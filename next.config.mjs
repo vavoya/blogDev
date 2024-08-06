ceb146709973eac4dd29b7599038bc7692caad83
vavoya <vavoya6324@gmail.com>
2024-08-07 05:48:55 +0900

    배포에 불필요한 파일 삭제

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,7 @@ const nextConfig = {
                 port: '',
             },
         ],
-    },
-    experimental: {
-        jsconfigPaths: true, // 이 플래그를 추가하여 jsconfig.json/tsconfig.json 경로 별칭을 활성화합니다.
-    },
+    }
 };
 
 export default nextConfig;
